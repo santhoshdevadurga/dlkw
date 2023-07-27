d023f1c1704bf5543c5e12730f911c758ae3e31d
santhoshdevadurga <105797457+santhoshdevadurga@users.noreply.github.com>
2023-07-27 15:02:08 +0530

    Update streamlit_app.py
    Snowflake connector

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
+import snowflake.connector
+import pandas
 
 streamlit.title('Zena\'s Amazing Athleisure Catalog')
+
+# connect to snowflake
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
